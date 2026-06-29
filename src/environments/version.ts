04beb164f8f7c3e9b8069cf2ce9359587e5c1da9
nkonko <nicoazzara@hotmail.com>
2026-06-29 17:01:35 +0200

    chore: bump version to 1.0.5

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,4 +2,4 @@
  * Version del proyecto, actualizada por create-release.mjs al hacer una release.
  * No modificar manualmente.
  */
-export const version = '1.0.4';
+export const version = '1.0.5';
